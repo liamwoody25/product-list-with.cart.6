57fa59b39f68e684700d3c7bcf4fe9ae27a8edcc
liam woodward <liamwoodward20@outlook.com>
2025-12-23 18:48:56 +0000

    adding localstorage to javascript

--- public/js/cart.js
+++ public/js/cart.js
@@ -12,6 +12,9 @@ const deleteBtn =  document.querySelectorAll('.decrease-btn');
 function addItemToCart(i){
   const output = document.querySelectorAll('#output')[i];
   const result = Number(output.innerText) + 1;
+  let productItem = localStorage.getItem('addItemToCart');
+  
+  productItem = parseInt(productItem);
 
   if (result > 10 ){
     result = 0
@@ -23,7 +26,27 @@ function addItemToCart(i){
   imageCard[i].style.border = '2px solid #C73B0F'
   imgCard[i].style.border = '2px solid #C73B0F'
   cardImage[i].style.border = '2px solid #C73B0F'
+
+
+  if (productItem){
+    localStorage.setItem('addItemToCart', productItem + 1)
+    document.getElementById('cart-output').textContent = productItem + 1;
+  } else {
+    localStorage.setItem('addItemToCart', 1)
+    document.getElementById('cart-output').textContent = 1;
+  }
+  
 }
+
+function updateCart(){
+  let productItem = localStorage.getItem('addItemToCart')
+
+  if (productItem) {
+    document.getElementById('cart-ouput').textContent = productItem
+  }
+}
+
+updateCart()
 
 
 
@@ -50,18 +73,23 @@ function removeItemFromCart(i) {
 
 
 
+// this event listener is for the cart button when it is clicked
 for(let i = 0; i < cartSubmit.length; i++){
     cartSubmit[i].addEventListener('click', function(){
       addItemToCart(i)
   })
 }
 
+
+// this event listener is for the increase button when it is clicked it makes the number go up
 for (let i = 0 ; i < insertBtn.length; i++) {
   insertBtn[i].addEventListener('click', function(){
     addItemToCart(i)
   })
 }
 
+
+// this event listener is for the decrease button when it is clicked it makes the number go down
 for(let i = 0; i < deleteBtn.length; i++) {
   deleteBtn[i].addEventListener('click', function(){
     removeItemFromCart(i)
