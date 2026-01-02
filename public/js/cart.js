db45a37021534cf70649a6aa2c55b924e8c020e1
liam woodward <liamwoodward20@outlook.com>
2026-01-02 10:17:26 +0000

    updating javascript

--- public/js/cart.js
+++ public/js/cart.js
@@ -7,62 +7,62 @@ const insertBtn = document.querySelectorAll('.increase-btn');
 const deleteBtn =  document.querySelectorAll('.decrease-btn');
 
 
-let cartItems = [
+let cartProducts = [
       {
-       "image": {
-            "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
-            "mobile": "./assets/images/image-waffle-mobile.jpg",
-            "tablet": "./assets/images/image-waffle-tablet.jpg",
-            "desktop": "./assets/images/image-waffle-desktop.jpg"
-       },
+      //  "image": {
+      //       "thumbnail": "./assets/images/image-waffle-thumbnail.jpg",
+      //       "mobile": "./assets/images/image-waffle-mobile.jpg",
+      //       "tablet": "./assets/images/image-waffle-tablet.jpg",
+      //       "desktop": "./assets/images/image-waffle-desktop.jpg"
+      //  },
        "name": "Waffle with Berries",
        "category": "Waffle",
        "price": 6.50,
-       "incCart": 0
+       "inCart": 0
     },
     {
-        "image": {
-            "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
-            "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
-            "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
-            "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
-        },
-        "name": "Vanilla Bean Crème Brûlée",
+        // "image": {
+        //     "thumbnail": "./assets/images/image-creme-brulee-thumbnail.jpg",
+        //     "mobile": "./assets/images/image-creme-brulee-mobile.jpg",
+        //     "tablet": "./assets/images/image-creme-brulee-tablet.jpg",
+        //     "desktop": "./assets/images/image-creme-brulee-desktop.jpg"
+        // },
+       "name": "Vanilla Bean Crème Brûlée",
         "category": "Crème Brûlée",
         "price": 7.00,
         "inCart": 0
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
-            "mobile": "./assets/images/image-macaron-mobile.jpg",
-            "tablet": "./assets/images/image-macaron-tablet.jpg",
-            "desktop": "./assets/images/image-macaron-desktop.jpg"
-        },
+        // "image": {
+        //     "thumbnail": "./assets/images/image-macaron-thumbnail.jpg",
+        //     "mobile": "./assets/images/image-macaron-mobile.jpg",
+        //     "tablet": "./assets/images/image-macaron-tablet.jpg",
+        //     "desktop": "./assets/images/image-macaron-desktop.jpg"
+        // },
         "name": "Macaron Mix of Five",
         "category": "Macaron",
         "price": 8.00,
         "inCart": 0
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
-            "mobile": "./assets/images/image-tiramisu-mobile.jpg",
-            "tablet": "./assets/images/image-tiramisu-tablet.jpg",
-            "desktop": "./assets/images/image-tiramisu-desktop.jpg"
-        },
+        // "image": {
+        //     "thumbnail": "./assets/images/image-tiramisu-thumbnail.jpg",
+        //     "mobile": "./assets/images/image-tiramisu-mobile.jpg",
+        //     "tablet": "./assets/images/image-tiramisu-tablet.jpg",
+        //     "desktop": "./assets/images/image-tiramisu-desktop.jpg"
+        // },
         "name": "Classic Tiramisu",
         "category": "Tiramisu",
         "price": 5.50,
         "inCart": 0
      },
      {
-        "image": {
-            "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
-            "mobile": "./assets/images/image-baklava-mobile.jpg",
-            "tablet": "./assets/images/image-baklava-tablet.jpg",
-            "desktop": "./assets/images/image-baklava-desktop.jpg"
-        },
+        // "image": {
+        //     "thumbnail": "./assets/images/image-baklava-thumbnail.jpg",
+        //     "mobile": "./assets/images/image-baklava-mobile.jpg",
+        //     "tablet": "./assets/images/image-baklava-tablet.jpg",
+        //     "desktop": "./assets/images/image-baklava-desktop.jpg"
+        // },
         "name": "Pistachio Baklava",
         "category": "Baklava",
         "price": 4.00,
@@ -119,10 +119,12 @@ let cartItems = [
      }
 ]
 
+
 // this function sends the item to the cart
-function addItemToCart(cartItem,[i]){
+function addItemToCart(cartProduct,[i]){
   const output = document.querySelectorAll('#output')[i];
   const result = Number(output.innerText) + 1;
+
   let productItem = localStorage.getItem('addItemToCart')
 
   productItem = parseInt(productItem)
@@ -147,8 +149,29 @@ function addItemToCart(cartItem,[i]){
     localStorage.setItem('addItemToCart', 1)
     document.getElementById('cart-output').textContent = 1;
    }
+
+   updateProduct(cartProduct)
 }
 
+
+function updateProduct(cartProduct) {
+  console.log('my product is', cartProduct)
+  
+  let items = {
+    [cartProduct.category]: cartProduct
+  }
+  cartProduct.inCart = 1
+  localStorage.setItem('cartProductInCart', JSON.stringify(cartProduct))
+
+}
+
+
+
+
+
+
+
+// this function is for when the cart is being updated 
 function updateCart() {
   let productItem = localStorage.getItem('addItemToCart');
 
@@ -168,7 +191,6 @@ function removeItemFromCart(i) {
   const output = document.querySelectorAll('#output')[i];
   const result = Number(output.innerText) - 1;
 
-
   if (result < 0 ) {
     result = 0
   }
@@ -181,7 +203,6 @@ function removeItemFromCart(i) {
     imgCard[i].style.border = 'none'
     cardImage[i].style.border = 'none'
   }
-  
 }
 
 
@@ -190,7 +211,7 @@ function removeItemFromCart(i) {
 // this event listener is for the cart button when it is clicked
 for(let i = 0; i < cartSubmit.length; i++){
     cartSubmit[i].addEventListener('click', function(){
-      addItemToCart(cartItems,i)
+      addItemToCart(cartProducts[i],[i])
   })
 }
 
